9d09fd3a5dcdb91a95b1915d58ca42f8d59b1455
dipu843302 <dipu843302@gmail.com>
2021-07-07 21:47:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/scrollbar_shedule_meeting/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.scrollbar_shedule_meeting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
